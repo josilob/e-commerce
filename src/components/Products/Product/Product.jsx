f58736366492ba9cf06bde1abfbae0db4094232a
josilob <bjosilo@gmail.com>
2021-03-24 16:23:36 -0400

    compiled and rendered successfuly

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import {Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core'
-import {AddShoppingCart} from '@material-ui/icons'
-import classes from '*.module.css'
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
 
-import useStyles from './styles'
+import useStyles from './styles';
 
 const Product = ({product}) => {
   const classes = useStyles();
+
   return (
     <div>
       <Card className={classes.root}>
         <CardMedia className={classes.media} image='' title={product.name}/>
+
         <CardContent>
+
           <div className={classes.CardContent}>
+
             <Typography variant='h5' gutterBottom>
               {product.name}
             </Typography>
+
             <Typography variant='h5' >
               {product.price}
             </Typography>
+
           </div>
           <Typography variant='h2' color='textSecondary'>{product.description}</Typography>
         </CardContent>
--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -1,7 +1,16 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(()=>({
-  root:{
-    maxWidth:'100%'
-  }
-}))
+export default makeStyles(() => ({
+	root: {
+		maxWidth: '100%',
+	},
+	media: {
+		height: 0,
+		paddingTop: '56.25%', //16:9
+	},
+	cardActions: {
+		display: 'flex',
+		justifyContent: 'flex-end',
+	},
+	cardContent: { display: 'flex', justifyContent: 'space-between' },
+}));
