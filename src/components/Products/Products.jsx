a9b5d4e8ab21a0c1004b1950e56e5a77252580b8
josilob <bjosilo@gmail.com>
2021-03-24 17:13:38 -0400

    product styles fix

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Grid} from '@material-ui/core'
 
 import Product from './Product/Product'
+import useStyles from './styles'
 
 const products = [
   {id:1, name:'Shoes',description:'Running shoes', price:'$45', image: 'https://cdn.shopify.com/s/files/1/0558/4169/products/Dark_Grey_Runner_1024x683_00001_9295631e-7c28-4da5-a7f8-1a1a5e30101c.jpg?v=1575470887'},
@@ -9,14 +10,18 @@ const products = [
 ]
 
 const Products = () => {
+  const classes = useStyles()
   return (
-  <main>
+   <main className={classes.content}>
+    <div className={classes.toolbar} />
+
     <Grid container justify='center' spacing={4}>
       {products.map((product)=>(
       <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
         <Product product={product}/>
       </Grid>))}
     </Grid>
+
   </main>
  )
 }
