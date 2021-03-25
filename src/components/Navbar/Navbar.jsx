c9d040ffe6e3c74c6269897ecccc294062b2aa03
josilob <bjosilo@gmail.com>
2021-03-24 22:29:17 -0400

    gitignore api key

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import logo from '../../assets/ecommerce-logo.jpeg'
 import useStyles from './styles'
 
 
-const Navbar = () => {
+const Navbar = ({totalItems}) => {
   const classes = useStyles()
   return (
     <React.Fragment>
@@ -14,12 +14,12 @@ const Navbar = () => {
         <Toolbar>
           <Typography variant='h6' className={classes.title} color='inherit'>
             <img src={logo} alt='Commerce.js' height='25px' className={classes.image}/>
-            CommerceApp
+            E-Commerce Project
           </Typography>
           <div className={classes.grow} />
           <div className={classes.button}>
             <IconButton aria-label='Show cart items' color='inherit'>
-              <Badge badgeContent={2} color='secondary'>
+              <Badge badgeContent={totalItems} color='secondary'>
                 <ShoppingCart/>
               </Badge>
             </IconButton>
