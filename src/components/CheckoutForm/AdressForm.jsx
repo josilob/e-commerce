7a489d096d1dd73fc93d0ba928cf20aa226ec3be
josilob <bjosilo@gmail.com>
2021-03-26 14:17:50 -0400

    Checkout form with react-hook-form

--- src/components/CheckoutForm/AdressForm.jsx
+++ src/components/CheckoutForm/AdressForm.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import {InputLabel, Select, MenuItem, Button, Grid, Typography} from '@material-ui/core'
+import {useForm, FormProvider} from 'react-hook-form'
+
+import FormInput from './CustomTextField'
 
 const AdressForm = () => {
+  const methods = useForm()
+
+
   return (
-    <div>
-      AdressForm
-    </div>
+    <React.Fragment>
+      <Typography variant='h6' gutterBottom>Shipping Adress</Typography>
+      <FormProvider {...methods}>
+        <form onSubmit=''>
+          <Grid container spacing={3}>
+            <FormInput required name='firstName' label='First name'/>
+            <FormInput required name='lastName' label='Last name'/>
+            <FormInput required name='adress1' label='Adress'/>
+            <FormInput required name='email' label='Email'/>
+            <FormInput required name='city' label='City'/>
+            <FormInput required name='zip' label='Postal code'/>
+          </Grid>
+        </form>
+      </FormProvider>
+    </React.Fragment>
   )
 }
 
