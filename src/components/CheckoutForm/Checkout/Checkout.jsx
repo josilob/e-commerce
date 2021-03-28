8a0bed3d435773c54265d732562dde0944ecd9f1
josilob <bjosilo@gmail.com>
2021-03-28 13:35:33 -0400

    checkout adress fixed and order summary generated

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,35 +1,42 @@
-import React, {useState, useEffect} from 'react'
-import {Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button} from '@material-ui/core'
+import React, { useState, useEffect } from 'react';
+import { CssBaseline, Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+import { Link, useHistory } from 'react-router-dom';
 
-import {commerce} from '../../../lib/commerce'
-import useStyles from './styles'
-import AdressForm from '../AdressForm'
-import PaymentForm from '../PaymentForm'
+import { commerce } from '../../../lib/commerce';
+import AddressForm from '../AdressForm';
+import PaymentForm from '../PaymentForm';
+import useStyles from './styles';
 
 const steps = ['Shipping adress', 'Payment details']
 
-const Checkout = ({cart}) => {
-  const [activeStep, setActiveStep] = useState(0)
-  const [checkoutToken,setCheckoutToken] = useState(null)
-  const [shippingData, setShippingdata] = useState({})
-  const classes = useStyles()
+const Checkout = ({ cart, onCaptureCheckout, order, error }) => {
+  const [checkoutToken, setCheckoutToken] = useState(null);
+  const [activeStep, setActiveStep] = useState(0);
+  const [shippingData, setShippingData] = useState({});
+  const classes = useStyles();
+  const history = useHistory();
 
-  useEffect(()=>{
-    const generateToken = async()=>{
-      try{
-        const token = await commerce.checkout.generateToken(cart.id, {type:'cart'})
-        console.log(token);
-        setCheckoutToken(token)
-      } catch (error){console.log(error)}
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+  useEffect(() => {
+    if (cart.id) {
+      const generateToken = async () => {
+        try {
+          const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
+
+          setCheckoutToken(token);
+        } catch {
+          if (activeStep !== steps.length) history.push('/');
+        }
+      };
+      generateToken();
     }
-    generateToken()
-  },[cart])
+  }, [cart]);
 
-  const nextStep = ()=> setActiveStep((previousActiveStep)=>previousActiveStep + 1)
-  const backStep = ()=> setActiveStep((previousActiveStep)=>previousActiveStep - 1)
 
   const next = (data) => {
-    setShippingdata(data)
+    setShippingData(data)
     nextStep()
   }
 
@@ -39,7 +46,9 @@ const Checkout = ({cart}) => {
     </div>
   )
 
-  const Form =()=> activeStep === 0 ? <AdressForm checkoutToken={checkoutToken} next={next}/> : <PaymentForm shippingData={shippingData}/>
+  const Form = () => (activeStep === 0
+    ? <AddressForm checkoutToken={checkoutToken} nextStep={nextStep} setShippingData={setShippingData} next={next} />
+    : <PaymentForm checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} shippingData={shippingData} />);
 
   return (
     <React.Fragment>
