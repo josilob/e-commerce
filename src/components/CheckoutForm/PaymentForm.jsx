98313c486b955897385e83d32fa7ae13ff0ed19a
josilob <bjosilo@gmail.com>
2021-03-28 14:03:38 -0400

    stripe implementation

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -4,12 +4,65 @@ import {Elements, CardElement, ElementsConsumer} from '@stripe/react-stripe-js'
 import {loadStripe} from '@stripe/stripe-js'
 
 import Review from './Review'
+import { CompareArrowsOutlined } from '@material-ui/icons'
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
 
 const PaymentForm = ({checkoutToken, nextStep, backStep, shippingData}) => {
+
+  const handleSubmit = (e, elements, stripe) => {
+    e.preventDefault()
+    if(!stripe || !elements) return
+
+    const cardElement = elements.getElement(CardElement)
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({ type: 'card', card: cardElement });
+
+    if(error){
+      console.log(error)
+      } else {
+      const orderData = {
+        line_items:checkoutToken.live.line_items,
+        customer: {firstName:shippingData.firstName, lastName: shippingData.lastName, email: shippingData.email},
+        shipping: {
+          name: 'Primary',
+          street: shippingData.address1,
+          town_city: shippingData.city,
+          county_state: shippingData.shippingSubdivision,
+          postal_zip_code: shippingData.zip,
+          ountry: shippingData.shippingCountry,
+        },
+        fulfillment: {shipping_method: shippingData.shippingOption},
+        payment: {
+          gateway: 'stripe',
+          stripe: {
+            payment_method_id: paymentMethod.id
+          }
+        }
+      }
+      
+    }
+  }
 
   return (
     <React.Fragment>
       <Review checkoutToken={checkoutToken}/>
+      <Divider />
+      <Typography variant='h6' gutterBottom style={{margin:'20px 0'}}>Payment method</Typography>
+      <Elements stripe={stripePromise}>
+        <ElementsConsumer>
+          {(elements, stripe)=>(
+            <form onSubmit={(e)=>{handleSubmit(e, elements, stripe)}}>
+              <CardElement/>
+              <br/><br/>
+              <div style={{display:'flex', justifyContent:'space-between'}}>
+                <Button variant='outlined' onClick={backStep}>Back</Button>
+                <Button type='submit' variant='contained' disabled={!stripe} color='primary'>Pay {checkoutToken.live.subtotal.formatted_with_symbol}</Button>
+              </div>
+            </form>
+          )}
+        </ElementsConsumer>
+      </Elements>
     </React.Fragment>
   )
 }
